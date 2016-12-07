d900ce56af44bd29f5184dedc73f096365884899
Andreas Orthey <andreas.orthey@gmx.de>
2016-12-07 17:10:46 +0900

    refactor

--- src/mc_myfirst_controller.cpp
+++ src/mc_myfirst_controller.cpp
@@ -8,62 +8,191 @@ namespace mc_control
                   : MCController(robot_module, dt)
         {
 
-                LOG_SUCCESS("MCMyFirstController robot loading " << robot().name());
                 this->info();
 
+                //*************************************************************
+                // check that right robot is loaded
+                //*************************************************************
+                LOG_SUCCESS("MCMyFirstController robot loading " << robot().name());
+                if(robot().name() != "hrp4"){
+                        LOG_ERROR("This controller does not know how to handle the robot you are controlling (" << robot().name() << ")")
+                        exit(0);
+                }
+
+                //*************************************************************
                 qpsolver->addConstraintSet(contactConstraint);
                 qpsolver->addConstraintSet(dynamicsConstraint);
-                //qpsolver->addConstraintSet(kinematicsConstraint);
+                qpsolver->addConstraintSet(kinematicsConstraint);
 
+                //*************************************************************
                 //Posture task
+                //*************************************************************
                 postureTask->stiffness(1.);
                 qpsolver->addTask(postureTask.get());
+
+                
+                //*************************************************************
                 //COM task
+                //*************************************************************
                 //comTask = std::make_shared<mc_tasks::CoMTask>(robots(), robots().robotIndex(), 2.0, 100.);
                 //qpsolver->addTask(comTask);
 
+                //*************************************************************
                 //Stability Task
-                stableTask = std::make_shared<mc_tasks::StabilityTask>(robots());
-                qpsolver->addTask(stableTask);
+                //*************************************************************
+                // stableTask = std::make_shared<mc_tasks::StabilityTask>(robots());
+                // qpsolver->addTask(stableTask);
 
+                //*************************************************************
                 //Endeffector Task
-                efTask = std::make_shared<mc_tasks::EndEffectorTask>("R_HAND_J1_LINK", robots(), robots().robotIndex(), 5.0, 10.);
-                qpsolver->addTask(efTask);
+                //*************************************************************
+                //left_hand = std::make_shared<mc_tasks::EndEffectorTask>("L_HAND_J1_LINK", robots(), robots().robotIndex(), 5.0, 10.);
+                double stiffness = 5.0;
+                double task_weight = 10.0;
+                task_right_hand = std::make_shared<mc_tasks::EndEffectorTask>("R_F23_LINK",
+                        robots(), robots().robotIndex(),
+                        stiffness, task_weight);
+                qpsolver->addTask(task_right_hand);
+                task_left_hand = std::make_shared<mc_tasks::EndEffectorTask>("L_F23_LINK",
+                        robots(), robots().robotIndex(),
+                        stiffness, task_weight);
+                qpsolver->addTask(task_left_hand);
 
+                //*************************************************************
                 //Set Contact task
+                //*************************************************************
                 qpsolver->setContacts({
                         {robots(), 0, 1, "LeftFoot", "AllGround"},
                         {robots(), 0, 1, "RightFoot", "AllGround"},
                         //{robots(), 0, 1, "LeftHand", "AllGround"},
-
                 });
 
-                if(robot().name() == "hrp4"){
-                        head_joint_index = robot().jointIndexByName("NECK_P");
-                }else{
-                        LOG_ERROR("This controller does not know how to handle the robot you are controlling (" << robot().name() << ")")
-                        exit(0);
-                }
+
+                //this->moveJointByName("NECK_P");
+                //this->moveJointByName("NECK_Y");
+
                 
-                //solver().addConstraintSet(selfCollisionConstraint);
+                // qpsolver->addConstraintSet(selfCollisionConstraint);
+
+                // selfCollisionConstraint.addCollisions(qpsolver, {
+                //         {"RARM_LINK5", "RLEG_LINK2", 0.05, 0.01, 0.},
+                //         {"RARM_LINK6", "RLEG_LINK2", 0.05, 0.01, 0.},
+                //         {"RARM_LINK7", "RLEG_LINK2", 0.05, 0.01, 0.}
+                // });
                 
-                //selfCollisionConstraint.addCollisions(qpsolver, {
-                //    {"RARM_LINK5", "RLEG_LINK2", 0.05, 0.01, 0.},
-                //        {"RARM_LINK6", "RLEG_LINK2", 0.05, 0.01, 0.},
-                //            {"RARM_LINK7", "RLEG_LINK2", 0.05, 0.01, 0.}});
-                
-                this->ros_bridge = mc_rtc::ROSBridge::get_node_handle();
-                LOG_SUCCESS("MCMyFirstController init done " << this);
+                // this->ros_bridge = mc_rtc::ROSBridge::get_node_handle();
+                // LOG_SUCCESS("MCMyFirstController init done " << this);
 
 
         }
+
+        void MCMyFirstController::moveJointByName(std::string joint_name){
+                std::vector<std::vector<double>> cur_obj = postureTask->posture();
+                int jind = robot().jointIndexByName(joint_name);
+                cur_obj[jind][0] = robot().qu()[jind][0];
+                postureTask->posture(cur_obj);
+        }
+        bool MCMyFirstController::run()
+        {
+                bool ret = MCController::run();
+                //*************************************************************
+                // Posture Task: move joint J to position 
+                // (theta_des) \in R
+                //*************************************************************
+                this->moveJointByName("NECK_P");
+                this->moveJointByName("NECK_Y");
+                this->moveJointByName("L_ELBOW_P");
+
+                //*************************************************************
+                // Endeffectortask: Move endeffector EE to position
+                // (x,y,z) \in R^3 x SO(3) = SE(3)
+                //*************************************************************
+                sva::PTransformd desired_pose(Eigen::Vector3d(0.5, -0.5, 1.0));
+                task_right_hand->set_ef_pose(desired_pose);
+
+                std::cout<< "[EE TASK] right hand dist to target:" <<
+                        task_right_hand->eval().norm() <<std::endl;
+
+                //*************************************************************
+                // Move Contact Task
+                //*************************************************************
+                // if(!moved_com)
+                // {
+                //         if(comTask->eval().norm() < 5e-2 && comTask->speed().norm() < 1e-4)
+                //         {
+                //                 LOG_SUCCESS("Moved the CoM")
+                //                 moved_com = true;
+                //                 qpsolver->setContacts({
+                //                         mc_rbdyn::Contact(robots(), "RFullSole", "AllGround")
+                //                 });
+                //                 qpsolver->addTask(efTask);
+                //                 efTask->add_ef_pose(sva::PTransformd(Eigen::Vector3d(0.4, 0, 0)));
+                //         }
+                //         return ret;
+                // }
+                // if(moved_com && !moved_left_foot)
+                // {
+                //         if(efTask->eval().norm() < 5e-2 && efTask->speed().norm() < 1e-4)
+                //         {
+                //                 LOG_SUCCESS("Moved the left foot")
+                //                 moved_left_foot = true;
+                //                 qpsolver->setContacts({
+                //                 mc_rbdyn::Contact(robots(), "LFullSole", "AllGround"),
+                //                 mc_rbdyn::Contact(robots(), "RFullSole", "AllGround")
+                //                 });
+                //                 qpsolver->removeTask(efTask);
+                //                 auto com = comTask->com();
+
+                //                 com.x() =
+                //                 (robot().mbc().bodyPosW[robot().bodyIndexByName("LLEG_LINK5")].translation().x()
+                //                 +
+                //                 robot().mbc().bodyPosW[robot().bodyIndexByName("RLEG_LINK5")].translation().x())/2;
+
+                //                 com.y() = 0;
+                //                 comTask->com(com);
+                //         }
+                //         return ret;
+                // }
+                return ret;
+
+        }
+
+        void MCMyFirstController::reset(const ControllerResetData & reset_data)
+        {
+                //init task-COM to current COM
+                MCController::reset(reset_data);
+                //task_left_hand->reset();
+                task_right_hand->reset();
+                //right_hand_current_pose = task_right_hand->get_ef_pose();
+
+                //comTask->reset();
+                //stableTask->reset();
+
+                //comZero = rbd::computeCoM(robot().mb(), robot().mbc());
+                //transformZero = efTask->get_ef_pose();
+
+                // comTask = std::make_shared<mc_tasks::CoMTask>(robots(), 0);
+
+                //efTask = std::make_shared<mc_tasks::EndEffectorTask>("LLEG_LINK5", robots(), 0);
+                // comTask->com(comTask->com() + Eigen::Vector3d(0,
+                //                               -1*efTask->get_ef_pose().translation().y(),
+                //                               0));
+                //qpsolver->addTask(comTask);
+        }
+
+        // void MCMyFirstController::display_joints()
+        // void MCMyFirstController::display_bodies()
+        // void MCMyFirstController::display_surfaces()
         void MCMyFirstController::info()
         {
+        
+                //*************************************************************
                 //display robot info
-                std::cout << "----- INFO -----" << std::endl;
+                //*************************************************************
+                std::cout << "----- INFO START " << std::string(63, '-') << std::endl;
                 std::cout << "ROBOT:    " << robot().name() << std::endl;
                 const rbd::MultiBody mb = robot().mb();
-
+                //*************************************************************
                 std::cout << "#########################################" << std::endl;
                 std::cout << "#JOINTS : " << mb.nrJoints() << std::endl;
                 const std::vector< rbd::Joint > joints = mb.joints();
@@ -72,7 +201,7 @@ namespace mc_control
                 for( jiter = joints.begin(); jiter!=joints.end(); jiter++){
                         std::cout << " [" << jctr++ << "]:" << (*jiter).name() << std::endl;
                 }
-
+                //*************************************************************
                 std::cout << "#########################################" << std::endl;
                 std::cout << "#LINKS  : " << mb.nrBodies() << std::endl;
                 const std::vector< rbd::Body > bodies = mb.bodies();
@@ -82,6 +211,7 @@ namespace mc_control
                 for( biter = bodies.begin(); biter!=bodies.end(); biter++){
                         std::cout << " [" << bctr++ << "]:" << (*biter).name() << std::endl;
                 }
+                //*************************************************************
                 std::cout << "#########################################" << std::endl;
                 std::cout << "#SURFACES" << std::endl;
                 const std::map<std::string, mc_rbdyn::SurfacePtr> surfaces = robot().surfaces();
@@ -91,74 +221,10 @@ namespace mc_control
                         std::cout << " [" << sctr++ << "]:" << (*siter).first << std::endl;
                 }
                 std::cout << "#########################################" << std::endl;
+                //*************************************************************
 
 
-                std::cout << "----- INFO -----" << std::endl;
+                std::cout << "----- INFO END   " << std::string(63, '-') << std::endl;
         }
-        bool MCMyFirstController::run()
-        {
-                bool ret = MCController::run();
-                std::vector<std::vector<double>> cur_obj = postureTask->posture();
-
-                int head_joint_yaw = robot().jointIndexByName("NECK_P");
-                int head_joint_pitch = robot().jointIndexByName("NECK_Y");
-                cur_obj[head_joint_yaw][0] = robot().ql()[head_joint_yaw][0];
-                cur_obj[head_joint_pitch][0] = robot().ql()[head_joint_pitch][0];
-
-                postureTask->posture(cur_obj);
-                if(efTask->eval().norm() < 0.02) {
-                        switch_target(); 
-                }
-                return ret;
-        }
-
-        void MCMyFirstController::reset(const ControllerResetData & reset_data)
-        {
-                //init task-COM to current COM
-                MCController::reset(reset_data);
-
-                stableTask->reset();
-                //comTask->reset();
-                efTask->reset();
-                comZero = rbd::computeCoM(robot().mb(), robot().mbc());
-                transformZero = efTask->get_ef_pose();
-
-                target_left = true;
-                switch_target();
-        }
-
-        void MCMyFirstController::switch_target()
-        {
-                double posture_target;
-                /// Posture task
-                if(target_left) {
-                        posture_target = robot().qu()[head_joint_index][0];
-                } else {
-                        posture_target = robot().ql()[head_joint_index][0];
-                }
-                std::vector<std::vector<double>> cur_obj = postureTask->posture();
-                cur_obj[head_joint_index][0] = posture_target;
-                postureTask->posture(cur_obj);
-
-                /// EE task
-                sva::PTransformd move_eff(Eigen::Vector3d(0.5, 0.0, 1.0));
-                efTask->set_ef_pose(transformZero);
-                efTask->add_ef_pose(move_eff);
-
-                /// COM task
-                Eigen::Vector3d comMove(0., 0.0, 0.);
-                Eigen::Vector3d target;
-
-                if(target_left) {
-                        target = comZero + comMove;
-                }else{
-                        target = comZero - comMove;
-                }
-                //comTask->com(target);
-
-                target_left = !target_left;
-
-        }
-
 }
 
--- src/mc_myfirst_controller.h
+++ src/mc_myfirst_controller.h
@@ -2,7 +2,9 @@
 #include <mc_control/mc_controller.h>
 #include <mc_control/api.h>
 #include <mc_rtc/ros.h>
+#include <mc_solver/BoundedSpeedConstr.h>
 #include <mc_tasks/CoMTask.h>
+#include <mc_tasks/AddRemoveContactTask.h>
 #include <mc_tasks/EndEffectorTask.h>
 #include <mc_tasks/MoveContactTask.h>
 #include <mc_tasks/StabilityTask.h>
@@ -18,17 +20,42 @@ namespace mc_control
                         MCMyFirstController(std::shared_ptr<mc_rbdyn::RobotModule> robot, double dt);
                         virtual bool run() override;
                         virtual void reset(const ControllerResetData & reset_data) override;
-                        void switch_target();
                         void info();
+                        void moveJointByName(std::string joint_name);
                 private:
+                        std::shared_ptr<ros::NodeHandle> ros_bridge;
+
                         int head_joint_index;
                         bool target_left;
-                        std::shared_ptr<ros::NodeHandle> ros_bridge;
-                        std::shared_ptr<mc_tasks::CoMTask> comTask;
-                        std::shared_ptr<mc_tasks::EndEffectorTask> efTask;
-                        std::shared_ptr<mc_tasks::StabilityTask> stableTask;
                         Eigen::Vector3d comZero;
-                        sva::PTransformd transformZero;
+
+
+
+                        bool moved_left_foot = false;
+                        bool moved_left_foot_to_wp = false;
+                        // std::shared_ptr<mc_solver::BoundedSpeedConstr> bSpeedConstr;
+                        // std::shared_ptr<mc_tasks::AddRemoveContactTask> aRContactTask;
+                        bool removed_left_foot = false;
+                        bool added_left_foot = false;
+
+                        std::shared_ptr<mc_tasks::EndEffectorTask> task_left_hand;
+                        std::shared_ptr<mc_tasks::EndEffectorTask> task_right_hand;
+                        sva::PTransformd right_hand_current_pose;
+                        sva::PTransformd left_hand_current_pose;
+
+                        // std::shared_ptr<mc_tasks::CoMTask> comTask;
+                        // std::shared_ptr<mc_tasks::StabilityTask> stableTask;
+                        // std::shared_ptr<mc_tasks::MoveContactTask> mcTask;
+
+                        // std::shared_ptr<mc_tasks::EndEffectorTask> footTask;
+
+                        bool moved_com = false;
+                        bool moved_right_foot = false;
+
+
+
+
+
 
         };
 
